10bae6d7213dcfc4e43ded8156b100dbb61d3b36
Eddie Jung <ejung716@gmail.com>
2021-08-06 09:59:42 -0700

    Create Storyboards for ViewController and NavigationController. Setup Add button using UIBarButtonItem. Create UIAlertController with textField for user input. Submit item into the UITableViewController tableRow.

--- project456/ViewController.swift
+++ project456/ViewController.swift
@@ -7,11 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        title = "Shopping List"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "List", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        cell.textLabel?.font = UIFont(name: (cell.textLabel?.font.fontName)!, size: 20)
+        
+        return cell
+    }
+    
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Add Item", message: "Add your shopping list item.", preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] action in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        
+        let index = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [index], with: .automatic)
     }
 
 
